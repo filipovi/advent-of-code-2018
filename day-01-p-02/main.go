2ea7f06784cc76ebec8f058c8cd5c07332950f2c
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2018-12-18 10:25:34 +0100

    Day 01 p02

---
+++ day-01-p-02/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func retrieve(values []string) int {
+	var result int
+	sums := map[int]bool{0: false}
+
+	for {
+		for _, value := range values {
+			value, _ := strconv.Atoi(value)
+			result += value
+			if sums[result] {
+				return result
+			}
+
+			sums[result] = true
+		}
+	}
+
+	return 0
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var values []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		values = append(values, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	result := retrieve(values)
+
+	fmt.Println(result)
+}
